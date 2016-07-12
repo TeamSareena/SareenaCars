9fdc72e3147b0b063813c77a762a4fd84b761312
Stefandimitrov89 <Stefan.dimitrov.89@gmail.com>
2016-07-12 15:14:44 +0300

    Add files via upload

---
+++ SareenaCars/Models/Items/Bonuses/FuelBonus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SareenaCars.Models
+{
+    class FuelBonus : Item
+    {
+
+        public FuelBonus(int width, int height) : base(width, height)
+        {
+        }
+
+
+        public override void ChangePosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
