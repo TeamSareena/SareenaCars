f8d715c72e375936795e95718a053154203645a2
Stefandimitrov89 <Stefan.dimitrov.89@gmail.com>
2016-07-12 12:07:57 +0300

    Add files via upload

---
+++ SareenaCars/Models/Fuel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SareenaCars.Models
+{
+    class Fuel : Models.Timer
+    {
+        public override DateTime Finish
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override DateTime Start
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ SareenaCars/Models/Timer.cs
@@ -0,0 +1,15 @@
+﻿using SareenaCars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SareenaCars.Models
+{
+    public abstract class Timer : ITimer
+    {
+        public abstract DateTime Finish { get; set; }
+        public abstract DateTime Start { get; set; }
+    }
+}
