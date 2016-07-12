813bff6b3401c2d8af59eb6ac89d1bd336e656b7
Stefandimitrov89 <Stefan.dimitrov.89@gmail.com>
2016-07-12 12:12:24 +0300

    Add files via upload

---
+++ SareenaCars/Models/Vehicles/Motorcycle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SareenaCars
+{
+    public class Motorcycle : Enemy
+    {
+        public Motorcycle(int width, int height) : base(width, height)
+        {
+        }
+
+        public override void ChangePosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
